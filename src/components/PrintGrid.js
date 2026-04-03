da49eda004803a599f2be88faa33a0ff42a6908b
Jay Farschman <jfarschman@gmail.com>
2026-04-03 12:56:50 -0600

    feature: better definition of cutlines

--- src/components/PrintGrid.js
+++ src/components/PrintGrid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CardPreview from './CardPreview';
 
 export default function PrintGrid({ deck }) {
+  // Split the deck into chunks of 9 for 3x3 pages
   const chunks = [];
   for (let i = 0; i < deck.length; i += 9) {
     chunks.push(deck.slice(i, i + 9));
@@ -13,18 +14,33 @@ export default function PrintGrid({ deck }) {
         <div key={pageIdx} className="print-page">
           <div className="print-grid">
             {page.map((card) => (
-              <div key={card.id} className="relative">
-                {/* Crop Marks */}
-                <div className="absolute -top-1 -left-1 w-2 h-2 border-l border-t border-black/30" />
-                <div className="absolute -top-1 -right-1 w-2 h-2 border-r border-t border-black/30" />
-                <div className="absolute -bottom-1 -left-1 w-2 h-2 border-l border-b border-black/30" />
-                <div className="absolute -bottom-1 -right-1 w-2 h-2 border-r border-b border-black/30" />
+              <div key={card.id} className="relative w-[63mm] h-[89mm]">
                 
+                {/* --- SCISSOR GUIDES (Professional Crop Marks) --- */}
+                {/* These lines sit just outside the card and point along the cutting edge */}
+                
+                {/* Top Left Corner */}
+                <div className="absolute -top-[4mm] -left-[1px] w-[1px] h-[3mm] bg-gray-400" />
+                <div className="absolute -top-[1px] -left-[4mm] w-[3mm] h-[1px] bg-gray-400" />
+                
+                {/* Top Right Corner */}
+                <div className="absolute -top-[4mm] -right-[1px] w-[1px] h-[3mm] bg-gray-400" />
+                <div className="absolute -top-[1px] -right-[4mm] w-[3mm] h-[1px] bg-gray-400" />
+                
+                {/* Bottom Left Corner */}
+                <div className="absolute -bottom-[4mm] -left-[1px] w-[1px] h-[3mm] bg-gray-400" />
+                <div className="absolute -bottom-[1px] -left-[4mm] w-[3mm] h-[1px] bg-gray-400" />
+                
+                {/* Bottom Right Corner */}
+                <div className="absolute -bottom-[4mm] -right-[1px] w-[1px] h-[3mm] bg-gray-400" />
+                <div className="absolute -bottom-[1px] -right-[4mm] w-[3mm] h-[1px] bg-gray-400" />
+
+                {/* The Card Component */}
                 <CardPreview card={card} />
+                
               </div>
             ))}
           </div>
-          {/* Removed the separate page-break div from here */}
         </div>
       ))}
     </div>
